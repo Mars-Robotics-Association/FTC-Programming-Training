1be12f25347c31e68a1cfdeff46353529039a785
matthewgourash <matthewgourash@gmail.com>
2021-07-28 14:08:49 -0400

    update

--- FTC-New/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training/SimpleAutonomous.java
+++ FTC-New/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training/SimpleAutonomous.java
@@ -1,91 +1,48 @@
+//:)
+
 package org.firstinspires.ftc.teamcode.OpMode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.robotcore.internal.camera.delegating.DelegatingCaptureSequence;
-import org.firstinspires.ftc.teamcode.Core.Input.ControllerInput;
+//The name that you use in the line below will show up on the phone for you to select to run on the robot
+@Autonomous(name = "SimpleAutonomous" , group = "Competition")
 
-//import org.firstinspires.ftc.teamcode.Core.Input.ControllerInputListener;
-//import org.firstinspires.ftc.teamcode.Core.Robots.MecanumBaseControl;
-import static java.lang.Thread.sleep;
-
-//SimpleAutonomous (or whatever name is in the line below) shows up on the phone so you can select it to run
-@Autonomous(name = "SimpleAutonomous", group = "Competition")
-@Disabled
-@Config
-//This name must match the file name
-public class SimpleAutonomous extends LinearOpMode //implements ControllerInputListener
-{
-    ////Dependencies////
-    //  private MecanumBaseControl control;
-
-    //declare wheel motors
+public class SimpleAutonomousv2 extends LinearOpMode {
+    //declare wheel motors.  The names you give your motors must match the names you give them in the configuration file that is stored on the phone.
     private DcMotor FR;//front right wheel
     private DcMotor FL;//front left wheel
-    private DcMotor RR;//rear right wheel
-    private DcMotor RL;//rear left wheel
-
-    // declare shooters, intake and feed
-    private DcMotor shooterLeft;
-    private DcMotor shooterRight;
-    private DcMotor intakeDcMotor;
-    private DcMotor feedDcMotor;
-
-    //delcare wobble lift and arms
-    private Servo wobbleLiftServo1;
-    private Servo wobbleLiftServo2;
-    private Servo wobbleLeftServo;
-    private Servo wobbleRightServo;
-
-    ////Variables////
-    //Tweaking Variables
-
-    private boolean hasRunForward = false;
+    private DcMotor RR;//back right wheel
+    private DcMotor RL;//back left wheel
 
     @Override
     public void runOpMode() throws InterruptedException {
-        //map the components to the robot configuration and set power on wheels to 0
+        //map the components to the robot configuration
         FR = hardwareMap.dcMotor.get("FR");
         FL = hardwareMap.dcMotor.get("FL");
         RR = hardwareMap.dcMotor.get("RR");
         RL = hardwareMap.dcMotor.get("RL");
 
+        //set the power on the motors/wheels to 0 so the robot doesn't start wandering right away.
         FR.setPower(0);
         FL.setPower(0);
         RR.setPower(0);
         RL.setPower(0);
 
-        shooterLeft = hardwareMap.dcMotor.get("shooterLeft");
-        shooterRight = hardwareMap.dcMotor.get("shooterRight");
-
-        wobbleLiftServo1 = hardwareMap.servo.get("wobbleLiftServo1");
-        wobbleLiftServo2 = hardwareMap.servo.get("wobbleLiftServo2");
-        wobbleLeftServo = hardwareMap.servo.get("wobbleLeftServo");
-        wobbleRightServo = hardwareMap.servo.get("wobbleRightServo");
-
-        intakeDcMotor = hardwareMap.dcMotor.get("intakeDcMotor");
-        feedDcMotor = hardwareMap.dcMotor.get("feedDcMotor");
-
-        waitForStart();
-        DriveForward();
-
+        waitForStart();//makes your robot wait until you press the arrow on the phone to start moving forward
+        DriveForward();//calls the DriveForward method that you will code below
     }
 
-    //START CODING THE DriveFoward() method after the "{" in the line below
     private void DriveForward() {
-
-
+        //PUT YOUR CODE TO MOVE THE ROBOT FORWARD HERE (BETWEEN THE BRACKETS BEFORE AND AFTER THIS LINE)
 
     }
 
+    //stop the robot after it moves forward the correct number of milliseconds
+    //PUT YOUR CODE TO STOP THE ROBOT HERE(BEFORE THE BRACKET BELOW)
 
 }
 }
--- FTC-New/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training/SimpleAutonomousKey.java
+++ FTC-New/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training/SimpleAutonomousKey.java
@@ -1,59 +1,26 @@
+//:)
+
 package org.firstinspires.ftc.teamcode.OpMode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.robotcore.internal.camera.delegating.DelegatingCaptureSequence;
-import org.firstinspires.ftc.teamcode.Core.Input.ControllerInput;
-
-//import org.firstinspires.ftc.teamcode.Core.Input.ControllerInputListener;
-//import org.firstinspires.ftc.teamcode.Core.Robots.MecanumBaseControl;
-import static java.lang.Thread.sleep;
-
-//SimpleAutonomous (or whatever name is here) shows up on the phone
-@Autonomous(name = "SimpleAutonomous", group = "Competition")
+@Autonomous(name = "SimpleAutonomous" , group = "Competition")
 @Disabled
-@Config
-//This name must match the file name
-public class SimpleAutonomousKey extends LinearOpMode //implements ControllerInputListener
-{
-    ////Dependencies////
-    //  private MecanumBaseControl control;
 
+public class SimpleAutonomousv2 extends LinearOpMode {
     //declare wheel motors
     private DcMotor FR;//front right wheel
     private DcMotor FL;//front left wheel
-    private DcMotor RR;//rear right wheel
-    private DcMotor RL;//rear left wheel
-
-    // declare shooters, intake and feed
-    private DcMotor shooterLeft;
-    private DcMotor shooterRight;
-    private DcMotor intakeDcMotor;
-    private DcMotor feedDcMotor;
-
-    //delcare wobble lift and arms
-    private Servo wobbleLiftServo1;
-    private Servo wobbleLiftServo2;
-    private Servo wobbleLeftServo;
-    private Servo wobbleRightServo;
-
-    ////Variables////
-    //Tweaking Vars
-
-    private int howManyLoops = 0;
-    private boolean hasRunForward = false;
+    private DcMotor RR;//back right wheel
+    private DcMotor RL;//back left wheel
 
     @Override
     public void runOpMode() throws InterruptedException {
-        //map the components to the robot configuration and set power on wheels to 0
+        //map the components to the robot configuration and set the power on wheels
         FR = hardwareMap.dcMotor.get("FR");
         FL = hardwareMap.dcMotor.get("FL");
         RR = hardwareMap.dcMotor.get("RR");
@@ -64,45 +31,21 @@ public class SimpleAutonomousKey extends LinearOpMode //implements ControllerInp
         RR.setPower(0);
         RL.setPower(0);
 
-        shooterLeft = hardwareMap.dcMotor.get("shooterLeft");
-        shooterRight = hardwareMap.dcMotor.get("shooterRight");
-
-        wobbleLiftServo1 = hardwareMap.servo.get("wobbleLiftServo1");
-        wobbleLiftServo2 = hardwareMap.servo.get("wobbleLiftServo2");
-        wobbleLeftServo = hardwareMap.servo.get("wobbleLeftServo");
-        wobbleRightServo = hardwareMap.servo.get("wobbleRightServo");
-
-        intakeDcMotor = hardwareMap.dcMotor.get("intakeDcMotor");
-        feedDcMotor = hardwareMap.dcMotor.get("feedDcMotor");
-
         waitForStart();
         DriveForward();
-
     }
 
-    /*@Override
-    public void start(){
-    }
+    private void DriveForward() {
+        long endRun = System.currentTimeMillis() + 8000;//define the time at which to stop as the current system time + 8 seconds
 
-    @Override
-    public void loop() {
-        if(!hasRunForward) {
-            DriveForward();
-        }
-    }
-*/
-    private void DriveForward() { howManyLoops++;
-        telemetry.addData("endRun LOOPS: ", howManyLoops);
-        telemetry.update();
-        long endRun = System.currentTimeMillis()+8000;//define the time at which to stop as the current system time + 3 seconds
-
-        while (endRun>System.currentTimeMillis()) {
-            FR.setPower(.25);
-            FL.setPower(-.25);
-            RR.setPower(.25);
-            RL.setPower(-.25);
-            telemetry.addData("endRun Countdown", endRun-System.currentTimeMillis());
+        while (endRun > System.currentTimeMillis()) {
+            FR.setPower(.5);
+            FL.setPower(-.5);
+            RR.setPower(.5);
+            RL.setPower(-.5);
+            telemetry.addData("endRun Countdown", endRun - System.currentTimeMillis());
             telemetry.update();
+
         }
 
         //stop
@@ -110,6 +53,6 @@ public class SimpleAutonomousKey extends LinearOpMode //implements ControllerInp
         FL.setPower(0);
         RR.setPower(0);
         RL.setPower(0);
-        hasRunForward = true;
+
     }
 }
