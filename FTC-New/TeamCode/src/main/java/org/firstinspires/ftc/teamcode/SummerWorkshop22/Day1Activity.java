a317bd6ab95cfac19eb4f4f88c9e9f4165301cda
Luke <lbkman36@gmail.com>
2022-08-01 18:58:28 -0400

    thing

--- FTC-New/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SummerWorkshop22/Day1Activity.java
+++ FTC-New/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SummerWorkshop22/Day1Activity.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 //The name that you use in the line below will show up on the phone for you to select to run on the robot
 @Autonomous(name = "LukeWantsToTestAThing" , group = "Competition")
 
-public class SimpleAutonomous extends LinearOpMode {
+public class Day1Activity extends LinearOpMode {
     //declare wheel motors.  The names you give your motors must match the names you give them in the configuration file that is stored on the phone.
     private DcMotor FR;//front right wheel
     private DcMotor FL;//front left wheel
@@ -31,18 +31,10 @@ public class SimpleAutonomous extends LinearOpMode {
         RL.setPower(0);
 
         waitForStart();//makes your robot wait until you press the arrow on the phone to start moving forward
-        SimpleMethods.moveWheels(FR,FL,RR,RL,100);
-        SimpleMethods.turnLeft(FR,FL,RR,RL,100);
-        //DriveForward();//calls the DriveForward method that you will code below
-        //this might break something
+
+        //write code here
     }
 
-    private void DriveForward() {
-        //PUT YOUR CODE TO MOVE THE ROBOT FORWARD HERE (BETWEEN THE BRACKETS BEFORE AND AFTER THIS LINE)
-        //This is a new comment
-    }
 
-    //stop the robot after it moves forward the correct number of milliseconds
-    //PUT YOUR CODE TO STOP THE ROBOT HERE(BEFORE THE BRACKET BELOW)
 
 }
