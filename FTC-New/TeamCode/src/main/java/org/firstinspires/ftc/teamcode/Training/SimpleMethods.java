ba436aad8f624c484ae5c90bb46ade7a247d7cdd
Luke <lbkman36@gmail.com>
2022-08-01 17:48:16 -0400

    stuff

---
+++ FTC-New/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training/SimpleMethods.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.Training;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class SimpleMethods {
+
+
+
+    public void moveWheels(DcMotor FR, DcMotor FL, DcMotor RR, DcMotor RL, int dist) {
+        FR.setTargetPosition(FR.getCurrentPosition()+dist);
+        FL.setTargetPosition(FL.getCurrentPosition()+dist);
+        RR.setTargetPosition(RR.getCurrentPosition()+dist);
+        RL.setTargetPosition(RL.getCurrentPosition()+dist);
+        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR.setPower(1);
+        FL.setPower(1);
+        RL.setPower(1);
+        RR.setPower(1);
+        while(FR.isBusy()||FL.isBusy()|| RR.isBusy()||RL.isBusy()){}
+        FR.setPower(0);
+        FL.setPower(0);
+        RR.setPower(0);
+        RL.setPower(0);
+    }
+
+    public void turnLeft(DcMotor FR, DcMotor FL, DcMotor RR, DcMotor RL, int dist){
+        FR.setTargetPosition(FR.getCurrentPosition()+dist);
+        FL.setTargetPosition(FL.getCurrentPosition()-dist);
+        RR.setTargetPosition(RR.getCurrentPosition()+dist);
+        RL.setTargetPosition(RL.getCurrentPosition()-dist);
+        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR.setPower(1);
+        FL.setPower(1);
+        RL.setPower(1);
+        RR.setPower(1);
+        while(FR.isBusy()||FL.isBusy()|| RR.isBusy()||RL.isBusy()){}
+        FR.setPower(0);
+        FL.setPower(0);
+        RR.setPower(0);
+        RL.setPower(0);
+    }
+}
+
